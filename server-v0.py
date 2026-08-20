7a7633c903614a0d29bb84daaf8b14d642bd0946
Foziljonov Abdulloh <foziljonovabdulloh64@gmail.com>
2026-08-20 18:22:59 +0500

    worked to prevent edge cases like when method is not GET and path is invalid

--- server-v0.py
+++ server-v0.py
@@ -2,10 +2,10 @@
 # 2) bind to (HOST, PORT) and listen
 # 3) loop: accept a connection
 # 4) recv bytes until you see b"\r\n\r\n" (end of HTTP headers)
-# 5) parse the request line: METHOD PATH HTTP_VERSION
-# 6) if METHOD != "GET": send 405 and close
-# 7) if PATH == "/": set PATH = "/index.html"
-# 8) map PATH to a file inside ./www (and prevent ../ traversal)
+# 5) parse the request line: method path HTTP_VERSION
+# 6) if method != "GET": send 405 and close
+# 7) if path == "/": set path = "/index.html"
+# 8) map path to a file inside ./www (and prevent ../ traversal)
 # 9) if file missing: send 404 and close
 # 10) if file exists:
 #       - send HTTP 200 headers (Content-Type + Content-Length, blank line)
@@ -14,6 +14,7 @@
 # 12) go back to accept next connection
 
 import socket
+from pathlib import Path
 
 HOST = "0.0.0.0"
 
@@ -41,11 +42,34 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
             headers = data[:end+4]
 
             request_line = data.split(b"\r\n")[0].split(b" ")
-            METHOD = request_line[0].decode()
-            PATH = request_line[1].decode()
+            method = request_line[0].decode()
+            path = request_line[1].decode()
             HTTP_VERSION = request_line[2].decode()
 
-            
+            if method!="GET":
+                response = (
+                    "HTTP/1.1 405 Method Not Allowed\r\n"
+                    "Content-Length: 0\r\n"
+                    "\r\n"
+                )
+
+                conn.sendall(response)
+                continue
+
+            if path == "/":
+                path = "/index.html"
+            www = Path("./www").resolve()
+            file_path = www / path.lstrip("/").resolve()
+
+            if not file_path.is_relative_to(www):
+                response = (
+                    "HTTP/1.1 403 Forbidden\r\n"
+                    "Content-Length: 0\r\n"
+                    "\r\n"
+                )
+
+                conn.sendall(response)
+                continue
 
 
 
