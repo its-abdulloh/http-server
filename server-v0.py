4976e88139e89c0d1e21eab9d2366fab20d4401a
Foziljonov Abdulloh <foziljonovabdulloh64@gmail.com>
2026-08-20 17:39:43 +0500

    loop accept a connection is done

--- server-v0.py
+++ server-v0.py
@@ -23,6 +23,16 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.bind((HOST,PORT))
     s.listen()
     print(f"Listening on ({HOST},{PORT})")
-    conn,addr = s.accept()
-    print(f"Connection accepted from ({addr})")
+    while True:
+        conn,addr = s.accept()
+        with conn:
+            print(f"Connection accepted from ({addr})")
+
+            while True:
+                data = conn.recv(1024)
+                if data:
+                    print(f"Recieved : {data.decode()}")
+                    conn.sendall(b"Greetings from the server!")
+                if not data:
+                    break
 
